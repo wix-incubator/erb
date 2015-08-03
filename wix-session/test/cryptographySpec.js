6d3f4c46d402b8d86fe81a9d4724d617d75f0cc9
kfirb <kfirb@wix.com>
2015-08-03 13:55:13 +0300

    wix session encode/decode

---
+++ wix-session/test/cryptographySpec.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var crypto = require('../index')({'mainKey': '1234567890123456'});
+var Chance = require('chance');
+
+var chance = new Chance();
+
+
+
+describe("cryptography", function () {
+    it("encrypt and decrypt", function () {
+        var key = chance.string({'length': 16});
+        var data = chance.string();
+        var encrypted = crypto.encrypt(data, key);
+        expect(crypto.decrypt(encrypted, key)).to.equal(data);
+    });
+});
