618e3e365279d66f0440ef70c1b96cc8f5d20ca1
kfirb <kfirb@wix.com>
2015-08-05 13:45:11 +0300

    experimenta - get list of buffers

--- json-rpc-client/lib/signer.js
+++ json-rpc-client/lib/signer.js
@@ -1,15 +1,26 @@
+var _ = require('lodash');
 
-exports.signString = function(str, key) {
-    return sign(new Buffer(str), key);
+exports.signString = function(strings, key) {
+    return sign(strings, key);
 };
 
-exports.signBuffer = function(buffer, key){
-    return sign(buffer, key);
+exports.signBuffer = function(buffers, key){
+    return sign(buffers, key);
 }
 
 
-var sign = function(buffer, key) {
+var sign = function(buffers, key) {
     var crypto = require('crypto')
-    var key = new Buffer(key, "utf8");
-    return crypto.createHmac('sha1', key).update(buffer).digest('hex');
+    var bufferedKey = new Buffer(key, "utf8");
+    var hmac = crypto.createHmac('sha1', bufferedKey);
+    if(_.isArray(buffers)){
+        _.forEach(buffers, function(n){
+            hmac.update(n);
+        });            
+    } else {
+        hmac.update(buffers);
+    }
+    
+
+    return hmac.digest('hex');
 };
--- json-rpc-client/test/signerSpec.js
+++ json-rpc-client/test/signerSpec.js
@@ -4,6 +4,7 @@ var signer = require('../lib/signer');
 
 var key = "1234567890123456";
 var text = "hi, I am going to be signed";
+var anotherText = "hi, i am another text";
 
 describe("signer", function () {
     it("sign string with HmacSHA1", function () {
@@ -11,5 +12,11 @@ describe("signer", function () {
     });
     it("sign buffer with HmacSHA1", function () {
         expect(signer.signBuffer(new Buffer(text), key)).to.equal("e9fe8a2f598bf3831bff2a62abbdbe200edcc7bb");
+    });
+    it("sign array of strings", function () {
+        expect(signer.signString([text, anotherText], key)).to.equal("df092169db51efdb3f707f89ae1a4ffaaf83077b");
+    });
+    it("sign array of buffers", function () {
+        expect(signer.signBuffer([new Buffer(text), new Buffer(anotherText)], key)).to.equal("df092169db51efdb3f707f89ae1a4ffaaf83077b");
     });
 });
