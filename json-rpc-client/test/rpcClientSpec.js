33111877473e8dd6253d6dbd2964d4ee5f23a14f
kfirb <kfirb@wix.com>
2015-08-09 17:05:45 +0300

    cleaning the tests

--- json-rpc-client/test/rpcClientSpec.js
+++ json-rpc-client/test/rpcClientSpec.js
@@ -6,7 +6,13 @@ var Promise = require('bluebird');
 var defaults = require('./defaults')();
 var driver = require('./drivers/rpcDriver');
 
-
+var tryAble = function (f) {
+    try {
+        f();
+    } catch (e) {
+        //do nothing
+    }
+};
 
 describe("rpc client", function () {
 
@@ -17,43 +23,46 @@ describe("rpc client", function () {
     });
 
     afterEach(function () {
-        try{
-            driver.stopServer();
-        }catch(e){
-            // Do nothing
-        }
+        tryAble(driver.stopServer);
     });
 
 
     it("send and get response from rpc client", function (done) {
-        var response = driver.rpcClientFor('/SomePath').invoke('add', [2,2]);
-        response.then(function(result) {
-            expect(result).to.equal(4);
-            done();
-        });
+        driver.rpcClientFor('/SomePath').invoke('add', [2, 2])
+            .then(function (result) {
+                expect(result).to.equal(4);
+                done();
+            });
     });
     it("send and get response from rpc client for function with no parameters", function (done) {
-        var response = driver.rpcClientFor('/SomePath').invoke('foo');
-        response.then(function(result) {
-            expect(result).to.equal('bar');
-            done();
-        });
+        driver.rpcClientFor('/SomePath').invoke('foo')
+            .then(function (result) {
+                expect(result).to.equal('bar');
+                done();
+            });
+    });
+    it("send request to not exists function", function (done) {
+        driver.rpcClientFor('/SomePath').invoke('notExistsFunction')
+            .catch(function (e) {
+                expect(e.message).to.equal("Method not found");
+                done();
+            })
     });
 
     it("invalid key", function (done) {
-        var response = driver.rpcClientFor('/SomePath', {key: 'some-invalid-key'}).invoke('add', [2,2]);
-        response.catch(function(result) {
-            expect(result.message).to.equal('invalid token');
-            done();
-        });
+        driver.rpcClientFor('/SomePath', {key: 'some-invalid-key'}).invoke('add', [2, 2])
+            .catch(function (result) {
+                expect(result.message).to.equal('invalid token');
+                done();
+            });
     });
     it("server is down, catch the promise", function (done) {
         driver.stopServer();
-        var response = driver.rpcClientFor('/SomePath').invoke('add', [2,2]);
-        response.catch(function(result) {
-            expect(result.message).to.equal('connect ECONNREFUSED');
-            done();
-        });
+        driver.rpcClientFor('/SomePath').invoke('add', [2, 2])
+            .catch(function (result) {
+                expect(result.message).to.equal('connect ECONNREFUSED');
+                done();
+            });
     });
 });
 
