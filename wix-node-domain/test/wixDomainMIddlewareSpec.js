7de11c3abcdd45fee80e66aedf013545f4337bca
kfirb <kfirb@wix.com>
2015-08-11 08:09:10 +0300

    Added Vilius comments regarding before and after

--- wix-node-domain/test/wixDomainMIddlewareSpec.js
+++ wix-node-domain/test/wixDomainMIddlewareSpec.js
@@ -1,19 +1,21 @@
-var request = require('request');
-var expect = require('chai').expect;
-var server = require('./testApp');
+var request = require('request'),
+    expect = require('chai').expect,
+    server = require('./testApp');
 
 var port = 3030;
 
-before(function () {
-    server.listen(port);
-});
 
-after(function () {
-    server.close();
-});
 
 describe("domain", function () {
 
+    before(function () {
+        server.listen(port);
+    });
+
+    after(function () {
+        server.close();
+    });
+    
     it("node domain", function (done) {
         request.get('http://localhost:' + port + '/domainName', function (error, response, body) {
             expect(body).to.equal('wix-domain');
