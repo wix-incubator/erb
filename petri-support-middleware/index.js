dcb78b3d48258deb539eadae0b8a47bcf6ac4148
kfirb <kfirb@wix.com>
2015-08-15 15:56:46 +0300

    move petri to test kit

--- petri-support-middleware/index.js
+++ petri-support-middleware/index.js
@@ -1,22 +1,19 @@
 var _ = require('lodash'),
     cookiesUtil = require('cookies-utils')();
 
-exports.init = function(app){
-    app.use(middleware());
-};
-
-
-var middleware = function () {
+exports.middleware = function () {
     return function (req, res, next) {
         var domain = require('wix-node-domain').wixDomain();
         var cookies = {};
         var reqCookies = cookiesUtil.toDomain(req.headers['cookie']);
 
-        Object.keys(reqCookies).map(function(key){
-            if(_.startsWith(key, "_wixAB3"))
+        Object.keys(reqCookies).map(function (key) {
+            if (_.startsWith(key, "_wixAB3"))
                 cookies[key] = reqCookies[key];
         });
         domain.petriCookies = cookies;
         next();
     };
 };
+
+
--- petri-support-middleware/test/petriMiddlewareSpec.js
+++ petri-support-middleware/test/petriMiddlewareSpec.js
@@ -1,49 +1,41 @@
 var request = require('request'),
     expect = require('chai').expect,
-    server = require('./testApp'),
+    server = require('http-test-kit').testApp(),
     _ = require('lodash'),
     cookiesUtils = require('cookies-utils')();
 
 var Chance = require('chance');
 var chance = new Chance();
 
-var toCookieString = function(cookies){
-    var cookieString = "";
-    for(var key in cookies){
-        cookieString += key + '=' + cookies[key] + '; ';
-    }
-    // TODO - remove this hack
-    return cookieString + "someOtherCookie=SomeValue; ";
-    
+server.getApp().use(require('wix-node-domain').wixDomainMiddleware());
+server.getApp().use(require('../index').middleware());
+
+server.getApp().get('/petriMiddlware', function (req, res) {
+    var domain = require('wix-node-domain').wixDomain();
+    res.send(domain.petriCookies);
+});
+
+
+var addCookie = function (cookies, name, value) {
+    cookies[name] = value;
 };
 
-var addCookie = function(cookies, name, value){
-    cookies[name] = value;        
-};
-
-describe("petri middleware", function(){
+describe("petri middleware", function () {
 
     var userId = chance.guid();
     var cookies = {};
-    
     addCookie(cookies, '_wixAB3', 'v1');
     addCookie(cookies, '_wixAB3' + userId, 'v2');
     addCookie(cookies, 'non-related-cookie', 'v3');
 
-    
+
     var port = 3333;
     var base_url = "http://localhost:" + port;
+
     
-    before(function () {
-        server.listen(port);
-    });
-
-    after(function () {
-        server.close();
-    });
-
+    server.beforeAndAfterEach();
+    
     it("send request with petri cookies and they should returned after take them from the domain", function (done) {
-
         var options = {
             uri: base_url + "/petriMiddlware",
             method: 'GET',
@@ -56,6 +48,6 @@ describe("petri middleware", function(){
             expect(JSON.parse(body)).to.deep.equal(cookies);
             done();
         });
-    });    
-    
+    });
+
 });
