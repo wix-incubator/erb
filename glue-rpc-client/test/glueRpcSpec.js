1096119875cfd91406c19f3da5997829beaa7b76
Kfir Bloch <kfirb@wix.com>
2015-08-15 16:41:30 +0300

    Update glueRpcSpec.js

--- glue-rpc-client/test/glueRpcSpec.js
+++ glue-rpc-client/test/glueRpcSpec.js
@@ -6,7 +6,7 @@ var chai = require('chai'),
 
 
 describe("glue rpc", function(){
-
+   // TODO - try sinom npm as STUB
     var rpcClient = function(){
         this.functions = [];
         this.registerHeaderBuildingHook = function(f){
