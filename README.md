5989e6506e8f9412a2424e9ead1a8fa06e628c35
yoav@wix.com <yoav@wix.com>
2015-08-16 11:34:46 +0300

    updated require of domain to be a singleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # server-platform-js
+
+To configure the local Wix NPM module use the instruction at http://kb.wixpress.com/pages/viewpage.action?title=Using+private+npm+registry&spaceKey=dashboard
--- wix-node-domain/index.js
+++ wix-node-domain/index.js
@@ -1,21 +1,22 @@
 var _ = require('lodash');
+var domain = require('domain');
 
 var domainName = 'wix-domain';
 
 exports.wixDomainMiddleware = function(){
   return function(req, res, next){
-      var domain = require('domain').create();
-      domain.name = domainName;
-      domain.req = req; /* TODO - no test - will be tested in glue npms */
-      domain.res = res; /* TODO - no test - will be tested in glue npms */
-      domain.run(function(){
+      var wixDomain = domain.create();
+      wixDomain.name = domainName;
+      wixDomain.req = req; /* TODO - no test - will be tested in glue npms */
+      wixDomain.res = res; /* TODO - no test - will be tested in glue npms */
+      wixDomain.run(function(){
          next();           
       });
   };      
 };
 
 exports.wixDomain = function(){
-    return _.find(require('domain')._stack, function(o){
+  return _.find(domain._stack, function(o){
         return o.name === domainName;
     });
 };
