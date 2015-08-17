6c0dc72a059737045e64064081981e2288eccbd4
Vilius Lukosius <vilius@wix.com>
2015-08-17 09:12:38 +0300

    Algned alignment

--- wix-config/src/wix-config.js
+++ wix-config/src/wix-config.js
@@ -4,52 +4,52 @@ import _ from 'lodash';
 var cachedConfig;
 
 const envKeys = new Map()
-    .set("APP_NAME", String)
-    .set("PORT", Number)
-    .set("MANAGEMENT_PORT", Number)
-    .set("MOUNT_POINT", String);
+  .set("APP_NAME", String)
+  .set("PORT", Number)
+  .set("MANAGEMENT_PORT", Number)
+  .set("MOUNT_POINT", String);
 
 export function get() {
-    if (!cachedConfig) {
-        cachedConfig = new WixConfig().config;
-    }
+  if (!cachedConfig) {
+    cachedConfig = new WixConfig().config;
+  }
 
-    return cachedConfig;
+  return cachedConfig;
 }
 
 class WixConfig {
-    constructor() {
-        this._config = this._loadAppConfig(process.env);
+  constructor() {
+    this._config = this._loadAppConfig(process.env);
 
-        if (!this._config.env)
-            this._config.env = this._loadEnvironment(process.env);
-        else {
-            this._config.env =  _.extend(this._loadEnvironment(process.env), this._config.env);
-        }
-
-        for (let key of envKeys.keys()) {
-            if (!this._config.env[_.camelCase(key)])
-                throw Error(`Could not load environment key for property '${key}'`);
-        }
+    if (!this._config.env)
+      this._config.env = this._loadEnvironment(process.env);
+    else {
+      this._config.env = _.extend(this._loadEnvironment(process.env), this._config.env);
     }
 
-    get config() {
-        return this._config;
+    for (let key of envKeys.keys()) {
+      if (!this._config.env[_.camelCase(key)])
+        throw Error(`Could not load environment key for property '${key}'`);
     }
+  }
 
-    _loadEnvironment(env) {
-        var config = {};
-        envKeys.forEach((converter, key) => {
-            if (env[key])
-                config[_.camelCase(key)] = converter(env[key]);
-        });
+  get config() {
+    return this._config;
+  }
 
-        return config;
-    }
+  _loadEnvironment(env) {
+    var config = {};
+    envKeys.forEach((converter, key) => {
+      if (env[key])
+        config[_.camelCase(key)] = converter(env[key]);
+    });
 
-    _loadAppConfig(env) {
-        let confDir = env.APP_CONF_DIR ? env.APP_CONF_DIR : "/configs";
-        let appName = _.last(env.APP_NAME.split("."));
-        return JSON.parse(fs.readFileSync(`${confDir}/${appName}-config.json`, 'utf8'));
-    }
+    return config;
+  }
+
+  _loadAppConfig(env) {
+    let confDir = env.APP_CONF_DIR ? env.APP_CONF_DIR : "/configs";
+    let appName = _.last(env.APP_NAME.split("."));
+    return JSON.parse(fs.readFileSync(`${confDir}/${appName}-config.json`, 'utf8'));
+  }
 }
--- wix-config/test/wixConfigSpec.js
+++ wix-config/test/wixConfigSpec.js
@@ -2,95 +2,95 @@ import { expect } from 'chai';
 
 describe("wix-config", () => {
 
-    beforeEach(() => {
-        unloadConfig();
-        resetEnvironment();
-    });
+  beforeEach(() => {
+    unloadConfig();
+    resetEnvironment();
+  });
 
-    it("caches config between imports", () => {
-        var wixConfig = require('../lib/index');
-        var config = wixConfig.get();
+  it("caches config between imports", () => {
+    var wixConfig = require('../lib/index');
+    var config = wixConfig.get();
 
-        delete process.env;
+    delete process.env;
 
-        expect(wixConfig.get()).to.deep.equal(config);
-    });
+    expect(wixConfig.get()).to.deep.equal(config);
+  });
 
-    it("loads values form environment", () => {
-        expect(loadConfig().env.port).to.be.equal(8080);
-    });
+  it("loads values form environment", () => {
+    expect(loadConfig().env.port).to.be.equal(8080);
+  });
 
-    it("loads config from default location", () => {
-        expect(loadConfig().cryptography.cipher.mainKey).to.be.equal("234234234");
-    });
+  it("loads config from default location", () => {
+    expect(loadConfig().cryptography.cipher.mainKey).to.be.equal("234234234");
+  });
 
-    it("loads 'APP_NAME' from environment", () => {
-        expect(loadConfig().env.appName).to.be.equal("com.wixpress.test");
-    });
+  it("loads 'APP_NAME' from environment", () => {
+    expect(loadConfig().env.appName).to.be.equal("com.wixpress.test");
+  });
 
-    it("loads 'PORT' from environment", () => {
-        expect(loadConfig().env.port).to.be.equal(8080);
-    });
+  it("loads 'PORT' from environment", () => {
+    expect(loadConfig().env.port).to.be.equal(8080);
+  });
 
-    it("loads 'MANAGEMENT_PORT' from environment", () => {
-        expect(loadConfig().env.managementPort).to.be.equal(8084);
-    });
+  it("loads 'MANAGEMENT_PORT' from environment", () => {
+    expect(loadConfig().env.managementPort).to.be.equal(8084);
+  });
 
-    it("loads 'MOUNT_POINT' from environment", () => {
-        expect(loadConfig().env.mountPoint).to.be.equal("/node");
-    });
+  it("loads 'MOUNT_POINT' from environment", () => {
+    expect(loadConfig().env.mountPoint).to.be.equal("/node");
+  });
 
-    it("does not override 'env' values form config", () => {
-        process.env.APP_NAME = "com.wixpress.test-with-env";
+  it("does not override 'env' values form config", () => {
+    process.env.APP_NAME = "com.wixpress.test-with-env";
 
-        let config = loadConfig();
+    let config = loadConfig();
 
-        expect(config.env.port).to.be.equal(3000);
-        expect(config.env.managementPort).to.be.equal(3004);
-        expect(config.env.appName).to.be.equal('app-name');
-        expect(config.env.mountPoint).to.be.equal('/mount-point');
-    });
+    expect(config.env.port).to.be.equal(3000);
+    expect(config.env.managementPort).to.be.equal(3004);
+    expect(config.env.appName).to.be.equal('app-name');
+    expect(config.env.mountPoint).to.be.equal('/mount-point');
+  });
 
-    it("merges values from config", () => {
-        process.env.APP_NAME = "com.wixpress.test-with-partial-env";
+  it("merges values from config", () => {
+    process.env.APP_NAME = "com.wixpress.test-with-partial-env";
 
-        let config = loadConfig();
+    let config = loadConfig();
 
-        expect(config.env.port).to.be.equal(3000);
-        expect(config.env.managementPort).to.be.equal(8084);
-        expect(config.env.appName).to.be.equal('com.wixpress.test-with-partial-env');
-        expect(config.env.mountPoint).to.be.equal('/node');
-    });
+    expect(config.env.port).to.be.equal(3000);
+    expect(config.env.managementPort).to.be.equal(8084);
+    expect(config.env.appName).to.be.equal('com.wixpress.test-with-partial-env');
+    expect(config.env.mountPoint).to.be.equal('/node');
+  });
 
-    it("fails only if environment variable is not present neither in env nor in config", () => {
-        process.env.APP_NAME = "com.wixpress.test-with-partial-env";
-        delete process.env.MANAGEMENT_PORT;
+  it("fails only if environment variable is not present neither in env nor in config", () => {
+    process.env.APP_NAME = "com.wixpress.test-with-partial-env";
+    delete process.env.MANAGEMENT_PORT;
 
-        expect(loadConfig).to.throw("Could not load environment key for property 'MANAGEMENT_PORT'");
+    expect(loadConfig).to.throw("Could not load environment key for property 'MANAGEMENT_PORT'");
 
-    });
+  });
 
-    it("fail to load if config is missing", function () {
-        process.env.APP_CONF_DIR = "./configsqwe";
-        expect(loadConfig).to.throw(/no such file or directory/);
-    });
+  it("fail to load if config is missing", function () {
+    process.env.APP_CONF_DIR = "./configsqwe";
+    expect(loadConfig).to.throw(/no such file or directory/);
+  });
 
-    function loadConfig() {
-        return require('../lib/index').get();
-    }
+  function loadConfig() {
+    return require('../lib/index').get();
+  }
 
-    function unloadConfig() {
-        delete require.cache[require.resolve('../lib/index')];
-    }
+  function unloadConfig() {
+    delete require.cache[require.resolve('../lib/index')];
+  }
 
-    function resetEnvironment() {
-        process.env = {
-            APP_NAME: "com.wixpress.test",
-            PORT: 8080,
-            MANAGEMENT_PORT: 8084,
-            MOUNT_POINT: "/node",
-            APP_CONF_DIR: "./test"
-        };
-    }
+  function resetEnvironment() {
+    process.env = {
+      APP_NAME: "com.wixpress.test",
+      PORT: 8080,
+      MANAGEMENT_PORT: 8084,
+      MOUNT_POINT: "/node",
+      APP_CONF_DIR: "./test"
+    };
+  }
 
 });
