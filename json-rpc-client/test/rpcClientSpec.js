efffd5178fba29a89a9860f2848685a3275dcf37
kfirb <kfirb@wix.com>
2015-08-22 11:09:26 +0300

    more test cleanup

--- json-rpc-client/test/rpcClientSpec.js
+++ json-rpc-client/test/rpcClientSpec.js
@@ -7,53 +7,57 @@ var driver = require('./drivers/rpcDriver');
 var port = 3000;
 var base_url = 'http://localhost:' + port;
 
-var urlFor = function(path){
+var urlFor = function (path) {
   return base_url + path;
 };
 
-var rpcFactory = function(key){
+var rpcFactory = function (key) {
   var defaults = require('./defaults');
   var signer = require('signer');
   var _key = key ? key : defaults().key;
   return  require('../index')(signer(_key));
 };
 
+var rpcClientFor = function(path, key){
+  var _rpcFactory = rpcFactory(key);
+  return _rpcFactory.rpcClient(urlFor(path));
+};
+
 var tryAble = function (f) {
-    try {
-        f();
-    } catch (e) {
-        //do nothing
-    }
+  try {
+    f();
+  } catch (e) {
+    //do nothing
+  }
 };
 
 describe("rpc client", function () {
 
-    beforeEach(function () {
-        driver.startServer();
-    });
+  beforeEach(function () {
+    driver.startServer();
+  });
 
-    afterEach(function () {
-        tryAble(driver.stopServer);
-    });
+  afterEach(function () {
+    tryAble(driver.stopServer);
+  });
 
 
-    it("send and get response from rpc client", function () {
+  it("send and get response from rpc client", function () {
+    return expect(rpcClientFor('/SomePath').invoke('add', 2, 2)).to.eventually.equal(4);
+  });
+  it("send and get response from rpc client for function with no parameters", function () {
+    return expect(rpcClientFor('/SomePath').invoke('foo')).to.eventually.equal('bar');
+  });
+  it("should be rejected because invoke not exists function", function () {
+    return expect(rpcClientFor('/SomePath').invoke('notExistsFunction')).to.be.rejected;
+  });
 
-        return expect(rpcFactory().rpcClient(urlFor('/SomePath')).invoke('add', 2, 2)).to.eventually.equal(4);
-    });
-    it("send and get response from rpc client for function with no parameters", function () {
-        return expect(rpcFactory().rpcClient(urlFor('/SomePath')).invoke('foo')).to.eventually.equal('bar');
-    });
-    it("should be rejected because invoke not exists function", function() {
-        return expect(rpcFactory().rpcClient(urlFor('/SomePath')).invoke('notExistsFunction')).to.be.rejected;
-    });
-
-    it("should be rejected because signer with invalid key", function () {
-        return expect(rpcFactory('invald-key').rpcClient(urlFor('/SomePath')).invoke('add', 2, 2)).to.be.rejected;
-    });
-    it("should be rejected because server is down", function () {
-        driver.stopServer();
-        return expect(rpcFactory().rpcClient(urlFor('/SomePath')).invoke('add', 2, 2)).to.be.rejected;
-    });
+  it("should be rejected because signer with invalid key", function () {
+    return expect(rpcClientFor('/SomePath', 'invalid-key').invoke('add', 2, 2)).to.be.rejected;
+  });
+  it("should be rejected because server is down", function () {
+    driver.stopServer();
+    return expect(rpcClientFor('/SomePath').invoke('add', 2, 2)).to.be.rejected;
+  });
 });
 
