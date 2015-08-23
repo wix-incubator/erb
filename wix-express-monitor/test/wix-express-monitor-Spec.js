37270f54f3a3ccc6a154aae70389f7440e62e783
yoav@wix.com <yoav@wix.com>
2015-08-23 15:29:11 +0300

    first validation of the monitoring package

--- wix-express-monitor/test/wix-express-monitor-Spec.js
+++ wix-express-monitor/test/wix-express-monitor-Spec.js
@@ -1,26 +1,49 @@
 var request = require('request');
 var expect = require('chai').expect;
 var serverResponsePatch = require('patch-server-response');
+var expressMonitor = require("../wix-express-monitor");
+var wixDomain = require('wix-node-domain');
 
 var port = 3030;
 var server = require('http-test-kit').testApp({port: port});
-server.getApp().get('/ok', function (req, res) {
+var testApp = server.getApp();
+
+var capturedMonitoringData;
+testApp.use(expressMonitor(function(monitor) {
+  capturedMonitoringData = monitor;
+}));
+testApp.use(wixDomain.wixDomainMiddleware());
+
+testApp.get('/ok', function (req, res) {
+  console.log("hi");
   res.write('hi');
   res.end();
 });
-server.getApp().get('/error', function (req, res) {
-  res.send('hi');
+testApp.get('/slow', function (req, res) {
+  res.append('an header', 'a value');
+  process.setTimeout(function() {
+    res.send("slow");
+  }, 10);
 });
-server.getApp().get('/timeout', function (req, res) {
+testApp.get('/timeout', function (req, res) {
   req.setTimeout(10, function() {
     res.emit("timeout");
   });
-  res.status(504).send("timeout");
 });
+testApp.get('/errorSync', function (req, res) {
+  throw new Error('Sync error');
+});
+testApp.get('/errorAsync', function (req, res) {
+  process.nextTick(function() {
+    throw new Error('Async error');
+  });
+});
+testApp.use(wixDomain.expressErrorHandlerMiddleware);
 
 
 
-describe("patch-server-response .patch()", function () {
+
+describe("wix monitor", function () {
 
   server.beforeAndAfter();
 
@@ -28,26 +51,19 @@ describe("patch-server-response .patch()", function () {
     serverResponsePatch.patch();
   });
 
-  it("should emit the before-writing-headers event when using send", function (done) {
-    request.get('http://localhost:' + port + '/send', function (error, response, body) {
-      expect(response.headers).to.have.property('x-before-flushing-headers', 'triggered');
+  it("should capture ok response", function (done) {
+    request.get('http://localhost:' + port + '/ok', function (error, response, body) {
+      expect(capturedMonitoringData).to.have.property('operationName', '/ok');
+      expect(capturedMonitoringData).to.have.property('start').that.is.a('number');
+      expect(capturedMonitoringData).to.have.property('timeToFirstByte').that.is.a('number');
+      expect(capturedMonitoringData).to.have.property('finish').that.is.a('number');
+      expect(capturedMonitoringData).to.have.property('timeout').that.is.an('undefined');
+      /*jshint -W030 */ // ignore the lint error of not having a function call. Mocha actually uses the .empty as a getter function call
+      expect(capturedMonitoringData).to.have.property('errors').that.is.empty;
       done();
     });
   });
 
-  it("should emit the before-writing-headers event when using write", function (done) {
-    request.get('http://localhost:' + port + '/write', function (error, response, body) {
-      expect(response.headers).to.have.property('x-before-flushing-headers', 'triggered');
-      done();
-    });
-  });
-
-  it("should emit the before-writing-headers event when using redirect", function (done) {
-    request.get('http://localhost:' + port + '/redirect', function (error, response, body) {
-      expect(response.headers).to.have.property('x-before-flushing-headers', 'triggered');
-      done();
-    });
-  });
 });
 
 
--- wix-express-monitor/wix-express-monitor.js
+++ wix-express-monitor/wix-express-monitor.js
@@ -7,7 +7,7 @@ module.exports = function expressMonitorFactory(onRequestCompleted) {
   return function expressMonitorMiddleware(req, res, next) {
     var start = process.hrtime();
     var metrics = {
-      operationName: req.route.path,
+      operationName: "",
       start: hrtimeToMilliSec(start),
       timeToFirstByte: undefined,
       finish: undefined,
@@ -15,7 +15,7 @@ module.exports = function expressMonitorFactory(onRequestCompleted) {
       errors: []
     };
 
-    res.on("before-writing-headers", function() {
+    res.on("x-before-flushing-headers", function() {
       metrics.timeToFirstByte = hrtimeToMilliSec(process.hrtime(start));
     });
 
@@ -23,13 +23,15 @@ module.exports = function expressMonitorFactory(onRequestCompleted) {
       metrics.timeout = hrtimeToMilliSec(process.hrtime(start));
     });
 
-    res.on("error", function(error) {
+    res.on("x-error", function(error) {
       metrics.errors.push(error)
     });
 
     res.on("finish", function() {
       metrics.finish = hrtimeToMilliSec(process.hrtime(start));
       metrics.statusCode = res.statusCode;
+      if (!metrics.operationName && req.route)
+        metrics.operationName = req.route.path;
       onRequestCompleted(metrics);
     });
 
