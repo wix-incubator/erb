60a99810612860c6c3bd76f5f536153a9fefa2db
kfirb <kfirb@wix.com>
2015-08-23 14:45:33 +0300

    added rpcSigner

--- json-rpc-client/test/drivers/matchers.js
+++ json-rpc-client/test/drivers/matchers.js
@@ -1,16 +1,24 @@
-
 module.exports = function (chai) {
 
-    chai.use(function (_chai, utils) {
-        _chai.Assertion.addMethod('beValidRpcRequest', function (id, method, params) {
-            var object = utils.flag(this, 'object');
-            var rpcObject = JSON.parse(object);            
-            new _chai.Assertion(rpcObject.jsonrpc).to.be.eql('2.0');
-            new _chai.Assertion(rpcObject.id).to.be.eql(id);
-            new _chai.Assertion(rpcObject.method).to.be.eql(method);
-            new _chai.Assertion(rpcObject.params).to.be.eql(params);
-        });
+  chai.use(function (_chai, utils) {
+    _chai.Assertion.addMethod('beValidRpcRequest', function (id, method, params) {
+      var object = utils.flag(this, 'object');
+      var rpcObject = JSON.parse(object);
+      new _chai.Assertion(rpcObject.jsonrpc).to.be.eql('2.0');
+      new _chai.Assertion(rpcObject.id).to.be.eql(id);
+      new _chai.Assertion(rpcObject.method).to.be.eql(method);
+      new _chai.Assertion(rpcObject.params).to.be.eql(params);
     });
+
+    _chai.Assertion.addMethod('haveSignature', function (sig, time) {
+      var object = utils.flag(this, 'object');
+      this.assert(
+        object['X-Wix-Signature'] === sig + ';' + time,
+        'expected #{this} to have signature header',
+        'expected #{this} to not have signature header'
+      );
+    });
+  });
 
 };
 
