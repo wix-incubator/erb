6dadf8260dbfe036761bb28698717949f8aa9da4
kfirb <kfirb@wix.com>
2015-08-24 15:26:11 +0300

    refactor rpc client

--- json-rpc-client/jsonRpcClient.js
+++ json-rpc-client/jsonRpcClient.js
@@ -1,19 +1,30 @@
 var request = require('request');
-var idGenerator = require('./lib/rpcRequestIdGenerator');
-var rpcProtocolSerializer = require('./lib/rpcProtocolSerializer')(idGenerator);
-var Promise = require('bluebird');
+var rpcProtocolSerializer = require('./lib/rpcProtocolSerializer')(require('./lib/rpcRequestIdGenerator'));
 var _ = require('lodash');
+var rpcSigner = require('./lib/rpcSigner');
 
 
+/**
+ * *
+ * @param signer object
+ * @returns {RpcClientFactory}
+ */
 module.exports = function rpc(signer) {
-  var rpcSigner = require('./lib/rpcSigner')(signer, function(){return Date.now();});
-  return new RpcClientFactory(rpcSigner);
+  return new RpcClientFactory(rpcSigner(signer, function () {
+    return Date.now();
+  }));
 };
 
 function RpcClientFactory(rpcSigner) {
   this.rpcSigner = rpcSigner;
 }
 
+/**
+ * *
+ * @param url for the service
+ * @param timeout in milliseconds
+ * @returns {RpcClient}
+ */
 RpcClientFactory.prototype.rpcClient = function (url, timeout) {
   return new RpcClient(url, timeout, this.rpcSigner);
 };
@@ -24,6 +35,12 @@ function RpcClient(url, timeout, rpcSigner) {
   this.rpcSigner = rpcSigner;
 }
 
+/**
+ * *
+ * @param method name
+ * @param params varargs of parameters
+ * @returns Promise of rpc response
+ */
 RpcClient.prototype.invoke = function (method, params) {
   return _invoke(this.url, this.rpcSigner, method, _.slice(arguments, 1))
 };
@@ -32,32 +49,38 @@ RpcClient.prototype.invoke = function (method, params) {
 function _invoke(url, rpcSigner, method, params) {
 
   var jsonRequest = rpcProtocolSerializer.serialize(method, params);
+
   var options = {
     uri: url,
     method: 'POST',
-    body: jsonRequest,
-    headers: {
-      'Content-Type': 'application/json-rpc',
-      'Accept': 'application/json-rpc'
-    }
+    body: jsonRequest
   };
 
-  rpcSigner.sign(jsonRequest, options.headers);
+  var headers = {};
+  addHeader(headers, 'Content-Type', 'application/json-rpc');
+  addHeader(headers, 'Accept', 'application/json-rpc');
+  rpcSigner.sign(jsonRequest, headers);
+  options.headers = headers;
 
-  /*
-   TODO - add catch for the json.parse
-   */
+
   return new Promise(function (resolve, reject) {
     request.post(options, function (error, response, body) {
-      if (error) {
-        reject(error);
-      } else if (!JSON.parse(body).error)
-        resolve(JSON.parse(body).result);
-      else {
-        reject(JSON.parse(body).error);
+      try {
+        if (error)
+          return reject(error);
+        var _body = JSON.parse(body);
+        if (_body.error)
+          reject(_body.error);
+        else
+          resolve(_body.result);
+      } catch (e) {
+        reject(e);
       }
     });
   });
 
 }
 
+var addHeader = function (hedears, name, value) {
+  hedears[name] = value;
+};
