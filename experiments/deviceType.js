365e8fb857206a69b59bc1bc44514511eb7d62de
kfirb <kfirb@wix.com>
2015-11-07 12:33:52 +0200

    play with require('useragent')

--- experiments/deviceType.js
+++ experiments/deviceType.js
@@ -1,11 +1,22 @@
 const parser = require('ua-parser-js');
 const assert = require('assert');
-const chrome = parser('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20');
+const useragent = require('useragent');
+const chromeString = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20';
+const chrome = parser(chromeString);
 
 assert(chrome.browser.name == 'Chrome');
 
-const googleBot = parser('Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)');
+const bingBotStr = 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)';
+const googleBot = parser(bingBotStr);
 
 // Not good, does not recognised bots :/ 
 assert(googleBot.browser.name == undefined);
 
+
+// Not the best, does not group it as bot
+var bingBotFromUserAgent = useragent.parse(bingBotStr);
+assert(bingBotFromUserAgent.family == 'bingbot');
+
+var chromeFromUserAgent = useragent.parse(chromeString);
+console.log(chromeFromUserAgent);
+assert(chromeFromUserAgent.family == 'Chrome');
