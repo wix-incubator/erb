25377b76157bf5ea864c9bb17ee220148b6f6573
Vilius Lukosius <vilius@wix.com>
2015-11-13 08:52:19 +0200

    Increased timeout for jvm build

--- testing/wix-jvm-bootstrap-testkit/test/wix-jvm-bootstrap-testkit.spec.js
+++ testing/wix-jvm-bootstrap-testkit/test/wix-jvm-bootstrap-testkit.spec.js
@@ -6,7 +6,7 @@ const bootstrapTestkit = require('..'),
   path = require('path');
 
 describe('jvm bootstrap testkit', function () {
-  this.timeout(60000);
+  this.timeout(180000);//ci takes long time to fetch java deps, as these are node build machines
 
   before(done => {
     try {
