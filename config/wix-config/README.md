7c5daa6aeb607cdfe5e658f750a9c94b99bec0d4
Vilius Lukosius <vilius@wix.com>
2015-12-10 00:13:13 +0200

    config/wix-config: simplified/cleaned-up

--- config/wix-config/README.md
+++ config/wix-config/README.md
@@ -1,6 +1,6 @@
 # wix-config
 
-Loads a file-based config from path defined by environment variable APP_CONF_DIR. Caches loaded config, so subsequent calls will not do fs access.
+Loads a file-based config from path defined by environment variable APP_CONF_DIR.
 
 ## install
 
@@ -11,9 +11,9 @@ npm install --save wix-config
 ## usage
 
 ```js
-const config = require('wix-config').get('app-name');
+const config = require('wix-config').load('app-name');
 ```
 
 ## Api
-### get(name)
+### load(name)
 Loads config from `${process.env.APP_CONF_DIR}/{name}.json`
--- config/wix-config/lib/wix-config.js
+++ config/wix-config/lib/wix-config.js
@@ -3,20 +3,14 @@ const fs = require('fs'),
   join = require('path').join,
   _ = require('lodash');
 
-const configs = {};
-
-module.exports.get = name => {
+module.exports.load = name => {
   if (_.isEmpty(name)) {
     throw new Error('config name must be provided and cannot be empty');
   }
 
-  if (!configs[name]) {
-    if (!process.env.APP_CONF_DIR) {
-      throw new Error('Environment variable "APP_CONF_DIR" is missing');
-    }
-
-    configs[name] = JSON.parse(fs.readFileSync(join(process.env.APP_CONF_DIR, `${name}.json`)));
-
+  if (!process.env.APP_CONF_DIR) {
+    throw new Error('Environment variable "APP_CONF_DIR" is missing');
   }
-  return configs[name];
+
+  return JSON.parse(fs.readFileSync(join(process.env.APP_CONF_DIR, `${name}.json`)));
 };
