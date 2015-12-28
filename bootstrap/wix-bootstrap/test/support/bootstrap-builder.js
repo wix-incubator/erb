6c9b852f1e7ab9455c5fa91cd39bfa463c707fc7
kfirb <kfirb@wix.com>
2015-12-28 14:51:12 +0200

    refactor test support

---
+++ bootstrap/wix-bootstrap/test/support/bootstrap-builder.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.bootstrapApp = (app, options) => new BootstrapApp(app, options);
+
+function BootstrapApp(app, options) {
+  const opts = _.merge({env: envSupport.basic()}, options || {});
+  const embeddedApp = testkit.embeddedApp(app, opts, testkit.checks.httpGet('/health/is_alive'));
+
+  this.beforeAndAfter = () => embeddedApp.beforeAndAfter();
+
+  this.getUrl = (path) => {
+    const completePath = join(opts.env.MOUNT_POINT, path || '');
+    return `http://localhost:${opts.env.PORT}${completePath}`;
+  };
+
+  this.getManagementUrl = (path) => {
+    const completePath = join(opts.env.MOUNT_POINT, path || '');
+    return `http://localhost:${opts.env.MANAGEMENT_PORT}${completePath}`;
+  };
+
+  this.stdout = () => embeddedApp.stdout();
+  this.stderr = () => embeddedApp.stderr();
+
+  this.env = opts.env;
+}
