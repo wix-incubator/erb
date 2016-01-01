16eed2d432328154856903dadc7cf43aa092c0d0
kfirb <kfirb@wix.com>
2016-01-01 16:35:23 +0200

    some of Vilius comments regarding the wix-session rpc support

--- rpc/wix-rpc-client-support/test/wix-session-enricher.spec.js
+++ rpc/wix-rpc-client-support/test/wix-session-enricher.spec.js
@@ -3,19 +3,19 @@ const chai = require('chai'),
   expect = chai.expect,
   wixSessionEnricher = require('../lib/enrichers/wix-session-enricher');
 
-describe('petri context', () => {
+describe('wix session enricher', () => {
 
-  it('should not have w-wix-session because session not exist', () =>{
+  it('should not have x-wix-session because session not exist', () =>{
     var headers = {};
     wixSessionEnricher.get(aWixSessionWith())(headers);
     expect(headers).to.not.have.property('X-Wix-Session');
   });
-  it('should not have w-wix-session because session exists but not token', () =>{
+  it('should not have x-wix-session because session exists but not token', () =>{
     var headers = {};
     wixSessionEnricher.get(aWixSessionWith({}))(headers);
     expect(headers).to.not.have.property('X-Wix-Session');
   });
-  it('should have w-wix-session because session and token exists', () =>{
+  it('should have x-wix-session because session and token exists', () =>{
     var headers = {};
     wixSessionEnricher.get(aWixSessionWith({token: 'some-token'}))(headers);
     expect(headers).to.have.property('X-Wix-Session', 'some-token');
