efddf89cc66f74e55b0821500908798c9b8042aa
kfirb <kfirb@wix.com>
2016-01-01 21:12:43 +0200

    request context - ip, port, url

--- aspects/wix-express-req-context/lib/wix-express-req-context.js
+++ aspects/wix-express-req-context/lib/wix-express-req-context.js
@@ -1,6 +1,8 @@
 'use strict';
 const _ = require('lodash'),
   reqContext = require('wix-req-context'),
+  remoteIpResolver = require('./remote-ip-resolver'),
+  remotePortResolver = require('./remote-port-resolver'),
   requestId = require('./requestId');
 
 module.exports = (req, res, next) => {
@@ -14,14 +16,11 @@ module.exports = (req, res, next) => {
     // TODO - Extract more parametets and save to context
     requestId: requestId.getOrCreate(req),
     userAgent: req.header('user-agent'),
-    url: _.find([req.header('X-WIX-URL'), req.originalUrl]),
+    url: _.find([req.header('x-wix-url'), req.originalUrl]),
     localUrl: req.originalUrl,
-    //app_url: undefined,
-    //local_ip: undefined,
-    //port: undefined,
-    // language: undefined,
-    //request_protocol: undefined
-    ip: req.ip
+    userPort: remotePortResolver.resolve(req),
+    // language: undefined TODO ,
+    userIp: remoteIpResolver.resolve(req)
   });
 
   next();
---
+++ aspects/wix-express-req-context/test/remote-ip.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+const expect = require('chai').expect,
+      remoteIpResolver = require('../lib/remote-ip-resolver');
+
+
+describe('remote ip', () =>{
+
+  var ip = '192.168.1.1';
+  var aRequest = () => {
+    return { headers: {}, connection: { remoteAddress: ip } };
+  };
+
+  it('shpuld return user ip from wix header', () => {
+    var request = aRequest();
+    var ipInWixHeader = '1.1.1.1';
+    request.headers = {'x-wix-ip': ipInWixHeader};
+    expect(remoteIpResolver.resolve(request)).to.be.equal(ipInWixHeader);
+  });
+  it('should return user ip from x-forward-for', () => {
+    var request = aRequest();
+    request.headers = { headers: {'x-forwarded-for': ip}};
+    expect(remoteIpResolver.resolve(request)).to.be.equal(ip);
+  });
+
+  it('should return ip from request', () => {
+    var ip = '192.168.1.1';
+    expect(remoteIpResolver.resolve(aRequest())).to.be.equal(ip);
+  });
+});
