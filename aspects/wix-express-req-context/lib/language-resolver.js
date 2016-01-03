4878de11f00a49223e8f83efaa993ffe26cd6664
kfirb <kfirb@wix.com>
2016-01-03 23:45:18 +0200

    refactor to use option monad

--- aspects/wix-express-req-context/lib/language-resolver.js
+++ aspects/wix-express-req-context/lib/language-resolver.js
@@ -1,57 +1,62 @@
 'use strict';
 
 const _ = require('lodash'),
-      cookieUtils = require('cookie-utils'),
-      acceptLanguage = require('accept-language-parser'),
-      urlParse  = require('url-parse');
+  cookieUtils = require('cookie-utils'),
+  acceptLanguage = require('accept-language-parser'),
+  urlParse = require('url-parse'),
+  option = require('option');
 
 
 exports.resolve = request => {
-
   return _.reduce([languageFromRpcHeader,
-                   languageFromDomain,
-                   languageFromWixHeader,
-                   languageFromCookie,
-                   languageFromAcceptLanguageHeader], (result, resolver) => {
-    return result || resolver(request);
-  }, null);
+    languageFromDomain,
+    languageFromWixHeader,
+    languageFromCookie,
+    languageFromAcceptLanguageHeader], (result, resolver) => {
+    return isNone(result) ? resolver(request) : result;
+  }, option.none).valueOrElse(null);
 
 };
 
+// Hack - i will do pull request for flatMap in option npm
+var isNone = (opt) => {
+  return opt.isNone() || option.fromNullable(opt.value()).isNone();
+};
 
-var languageFromCookie = request => cookieUtils.fromHeader(request.headers['cookie'])['wixLanguage'] || null;
+var languageFromCookie = request => {
+  return option.fromNullable(cookieUtils.fromHeader(request.headers['cookie'])['wixLanguage']);
+};
+
 
 var languageFromAcceptLanguageHeader = request => {
-  const val = request.headers['accept-language'];
-  if(val){
-    let parse = acceptLanguage.parse(val);
-    if(parse.length > 0){
-      return parse[0].code;
-    }
-  }
-  return null;
+  return option.fromNullable(request.headers['accept-language'])
+    .map((headerValue) => {
+      let parse = acceptLanguage.parse(headerValue);
+      return (parse.length > 0) ? parse[0].code : null;
+    });
 };
 
-var languageFromWixHeader = request =>{
-  const val = request.headers['x-wix-base-uri'];
-  if(val){
-    const url = urlParse(val);
-    if(url && url.host){
-      return langFromHost(url.host);
-    }
-  }
-  return null;
+var languageFromWixHeader = request => {
+  return option.fromNullable(request.headers['x-wix-base-uri'])
+    .map((headerValue) => {
+      const url = urlParse(headerValue);
+      return (url && url.host) ? langFromHost(url.host) : option.none;
+    });
 };
 
-var languageFromDomain  = request => langFromHost(request.get('host'));
+var languageFromDomain = request => {
+  return option.fromNullable(langFromHost(request.get('host')));
+};
 
-var languageFromRpcHeader = request => request.headers['x-wix-language'] || null;
+var languageFromRpcHeader = request => {
+  return option.fromNullable(request.headers['x-wix-language']);
+};
 
-var langFromHost = host =>{
+var langFromHost = host => {
   const index = host.indexOf('.');
-  if(index !== -1){
+  if (index !== -1) {
     const lang = host.substring(0, index);
-    if(lang.length === 2){
+    if (lang.length === 2) {
       return lang;
     }
   }
