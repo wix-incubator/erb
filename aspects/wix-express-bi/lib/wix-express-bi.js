f77d556b06749b1df3d174b263225058d914f111
kfirb <kfirb@wix.com>
2016-01-04 07:53:09 +0200

    init wix-express-bi

---
+++ aspects/wix-express-bi/lib/wix-express-bi.js
@@ -0,0 +1,2 @@
+'use strict';
+exports.hello = () => 'world';
---
+++ aspects/wix-express-bi/test/wix-bi.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('wix bi', ()=>{
+  
+  it('hi', ()=>{
+    expect(1).to.be.equal(1);
+  });
+
+
+});
