46a22e6e1e05c0ff79b811a90a6b7b37769d2fb6
kfirb <kfirb@wix.com>
2016-01-11 23:29:15 +0200

    another try to support proper head: http://www.giantflyingsaucer.com/blog/?p=3936

--- health/wix-express-isalive/lib/wix-express-isalive.js
+++ health/wix-express-isalive/lib/wix-express-isalive.js
@@ -2,7 +2,12 @@
 
 module.exports.addTo = app => {
   app.all('/health/is_alive', (req, res) => {
-    (req.method === 'HEAD') ? res.end() : res.send('Alive');
+    (req.method === 'HEAD') ? head(res) : res.send('Alive');
 
   });
+};
+
+var head = res =>{
+  res.writeHead(200, {'Content-Length': 0});
+  res.end('');
 };
--- health/wix-express-isalive/test/wix-express-isalive.spec.js
+++ health/wix-express-isalive/test/wix-express-isalive.spec.js
@@ -20,7 +20,7 @@ describe('wix health is alive', () => {
   it('respond with status: 200 for HEAD request', done => {
     request.head(server.getUrl('health/is_alive'), (err, res) => {
       expect(res.statusCode).to.equal(200);
-      expect(res.headers['content-length']).to.be.undefined;
+      expect(res.headers['content-length']).to.be.equal('0');
       done();
     });
   });
