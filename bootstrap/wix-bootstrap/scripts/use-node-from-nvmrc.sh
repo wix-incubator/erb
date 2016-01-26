c1bd1f0047a92411f92f2299f59e5304d4cac1ef
Vilius Lukosius <vilius@wix.com>
2016-01-26 09:59:54 +0200

    bootstrap/*: aligned with changes in https://github.com/wix/wix-node-docker-base

--- bootstrap/wix-bootstrap/scripts/use-node-from-nvmrc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Script to install node version defined in .nvmrc and replace global node with it.
-# Should be executed in app dir once app assets are copied and before npm install.
-#
-echo 'Replacing node version with the one in .nvmrc'
-. ~/.nvm/nvm.sh
-nvm install
-ln -sf `nvm which current` '/usr/local/bin/node'
