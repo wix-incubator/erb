71b63c76423e1b28645b826d4b3e250098147b25
Vilius Lukosius <vilius@wix.com>
2016-01-29 15:26:46 +0200

    aspects/wix-express-session: session is not populated if it has expired.

--- security/wix-session-crypto-testkit/test/wix-session-crypto-testkit.spec.js
+++ security/wix-session-crypto-testkit/test/wix-session-crypto-testkit.spec.js
@@ -7,8 +7,13 @@ const expect = require('chai').expect,
 describe('wix session crypto testkit', () => {
 
   it('should generate valid bundle', () => {
-    let bundle = testkit.aValidBundle();
+    const bundle = testkit.aValidBundle();
     expect(wixSessionCrypto.get(bundle.mainKey).encrypt(bundle.session)).to.equal(bundle.token);
+  });
+
+  it('should generate with expiration date in future', () => {
+    const bundle = testkit.aValidBundle();
+    expect(bundle.session.expiration.getTime()).to.be.gt(Date.now() + 60*60*1000);
   });
 
   it('should provide valid cookie name in newly generated bundle', () => {
@@ -16,7 +21,23 @@ describe('wix session crypto testkit', () => {
   });
 
   it('should provide valid sessionJson', () => {
-    let bundle = testkit.aValidBundle();
+    const bundle = testkit.aValidBundle();
     expect(bundle.sessionJson).to.deep.equal(JSON.parse(JSON.stringify(bundle.session)));
   });
+
+  it('should allow to provide custom encryption key (mainKey)', () => {
+    const bundle = testkit.aValidBundle({mainKey: '1234211331224111'});
+    expect(bundle.mainKey).to.equal('1234211331224111');
+    expect(wixSessionCrypto.get('1234211331224111').encrypt(bundle.session)).to.equal(bundle.token);
+  });
+
+  it('should allow to override session json fields', () => {
+    const bundle = testkit.aValidBundle({
+      session: {
+        userGuid: 'overriden_guid'
+      }
+    });
+    expect(bundle.session).to.contain.deep.property('userGuid', 'overriden_guid');
+  });
+
 });
