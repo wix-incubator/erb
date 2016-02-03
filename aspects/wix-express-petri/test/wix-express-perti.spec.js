c141c9aa779d63488ad9e45907fe7cdc0480fce3
kfirb <kfirb@wix.com>
2016-02-03 21:37:07 +0200

    rewrite petri cookies

--- aspects/wix-express-petri/test/wix-express-perti.spec.js
+++ aspects/wix-express-petri/test/wix-express-perti.spec.js
@@ -1,42 +1,54 @@
 'use strict';
 var request = require('request'),
-  expect = require('chai').expect,
+  chai = require('chai'),
+  expect = chai.expect,
   testkit = require('wix-http-testkit'),
   cookieUtils = require('cookie-utils'),
   domainMiddleware = require('wix-express-domain'),
   petriMiddleware = require('..'),
-  petri = require('wix-petri');
+  wixExpressReqContext = require('wix-express-req-context');
 
 describe('petri middleware', () => {
   const server = aServer();
 
+  chai.use(matchers);
+
   server.beforeAndAfter();
 
-  it('should extract petri cookies', done => {
+  it('should rewrite petri cookies to response', done => {
     let requestCookies = {
       '_wixAB3': 'v1',
       '_wixAB3|5754ef35-5f99-41ec-b01c-f781721': 'v2',
       'non-related-cookie': 'v3'
     };
 
-    let expectedCookies = {
-      '_wixAB3': 'v1',
-      '_wixAB3|5754ef35-5f99-41ec-b01c-f781721': 'v2'
-    };
+    var expectedCookies = [];
+    expectedCookies.push({name: '_wixAB3', value: 'v1'});
+    expectedCookies.push({name: '_wixAB3|5754ef35-5f99-41ec-b01c-f781721', value: 'v2'});
+
 
     request.get(withCookies(requestCookies), (error, response, body) => {
-      expect(JSON.parse(body)).to.deep.equal(expectedCookies);
+      expect(body).to.deep.equal('ok');
+
+      [0,1].forEach(index => {
+        expect(cookieFromHeader(response, index))
+          .to.be.aValidPetriCookie(expectedCookies[index].name, expectedCookies[index].value);
+      });
       done();
     });
   });
 
   it('should not fail without petri cookies in request', done => {
     request.get(server.getUrl(), (error, response, body) => {
-      expect(JSON.parse(body)).to.deep.equal({});
+      expect(body).to.equal('ok');
+      expect(response.headers).to.not.have.property('Set-Cookie');
       done();
     });
-
   });
+
+  var cookieFromHeader = (response, pos) => {
+    return cookieUtils.fromHeader(response.headers['set-cookie'][pos]);
+  };
 
   function withCookies(cookies) {
     return {
@@ -49,15 +61,30 @@ describe('petri middleware', () => {
 
   function aServer() {
     const server = testkit.server();
+    const wixPatchServerResponse = require('wix-patch-server-response');
+    wixPatchServerResponse.patch();
     const app = server.getApp();
 
     app.use(domainMiddleware);
+    app.use(wixExpressReqContext.get({seenByInfo: 'who-cares'}));
     app.use(petriMiddleware);
 
     app.get('/', (req, res) => {
-      res.send(petri.get());
+      res.send('ok');
     });
 
     return server;
   }
+
+  function matchers(chai) {
+    chai.Assertion.addMethod('aValidPetriCookie', aValidPetriCookie);
+
+    function aValidPetriCookie(name, value) {
+      var cookie = this._obj;
+      new chai.Assertion(cookie['Max-Age']).to.be.eql('15552000');
+      new chai.Assertion(cookie['Domain']).to.be.eql('.wix.com');
+      new chai.Assertion(cookie[name]).to.be.eql(value);
+    }
+  }
+
 });
