08c3cf2511ca41640d1b553da187e617d9d3683f
kfirb <kfirb@wix.com>
2016-02-14 12:53:22 +0200

    cache control

--- express/wix-express-caching-policy/lib/wix-express-caching-policy.js
+++ express/wix-express-caching-policy/lib/wix-express-caching-policy.js
@@ -2,16 +2,34 @@
 
 
 module.exports.defaultStrategy = () => middleware({});
-module.exports.specific = age => middleware({caching: "specific", age: age});
-module.exports.infinite = () => middleware({caching: "infinite"});
-module.exports.noHeaders = () => middleware({caching: "noHeaders"});
-module.exports.maxAge = () => middleware({caching: "maxAge"});
+module.exports.specific = age => middleware({caching: 'specific', age: age});
+module.exports.infinite = () => middleware({caching: 'infinite'});
+module.exports.noHeaders = () => middleware({caching: 'noHeaders'});
+module.exports.noCache = () => middleware({caching: 'noCache'});
 
 var middleware = strategy => (req, res, next) =>{
   req.cachingPolicy = strategy;
+
+
+  res.on('x-before-flushing-headers', () => {
+    switch (req.cachingPolicy.caching){
+      case undefined:
+        res.set('Pragma', 'no-cache');
+        res.set('Cache-Control', 'no-cache');
+        break;
+      case 'specific':
+        res.set('Cache-Control', 'max-age=' + req.cachingPolicy.age);
+        break;
+      case 'infinite':
+        res.set('Cache-Control', 'max-age=2419200');
+        break;
+      case 'noCache':
+        res.set('Pragma', 'no-cache');
+        res.set('Cache-Control', 'no-cache');
+        break;
+    }
+  });
+
   next();
-  
-  // here we listen to event and write headers
-  // need tp preserve the state, to make sure headers written only once
 };
 
--- express/wix-express-caching-policy/test/wix-patch-server-response.spec.js
+++ express/wix-express-caching-policy/test/wix-patch-server-response.spec.js
@@ -1,37 +1,94 @@
 'use strict';
 const request = require('request'),
-  expect = require('chai').expect,
+  chai = require('chai'),
   cp = require('..'),
   testkit = require('wix-http-testkit');
 
 describe('chaching policy', () => {
 
+  chai.use(matchers);
+  let expect = chai.expect;
+
   let server = aServer();
   server.beforeAndAfterEach();
 
-  it('bla bla', done => {
-    request.get(server.getUrl('/send'), (err, res) =>{
-      expect(res.statusCode).to.equal(200);
+  it('should return no cache for default', done => {
+    request.get(server.getUrl('/default'), (err, res) =>{
+      expect(res.headers).to.haveHeaders({pragma: 'no-cache', 'cache-control': 'no-cache'});
+      done();
+    });
+  });
+
+  it('should return specific age', done => {
+    request.get(server.getUrl('/specific'), (err, res) =>{
+      expect(res.headers).to.haveHeaders({'cache-control': 'max-age=1000'});
+      expect(res.headers).to.notHaveHeader(['pragma']);
+      done();
+    });
+  });
+
+  it('should return infinite', done => {
+    request.get(server.getUrl('/infinite'), (err, res) =>{
+      expect(res.headers).to.haveHeaders({'cache-control': 'max-age=2419200'});
+      expect(res.headers).to.notHaveHeader(['pragma']);
+      done();
+    });
+  });
+
+  it('should return no cache headers', done => {
+    request.get(server.getUrl('/noHeaders'), (err, res) =>{
+      expect(res.headers).to.notHaveHeader(['pragma', 'cache-control']);
+      done();
+    });
+  });
+
+  it('should return noCache', done => {
+    request.get(server.getUrl('/noCache'), (err, res) =>{
+      expect(res.headers).to.haveHeaders({pragma: 'no-cache', 'cache-control': 'no-cache'});
       done();
     });
   });
 
 
+  function matchers(chai) {
+    chai.Assertion.addMethod('haveHeaders', haveHeaders);
+    chai.Assertion.addMethod('notHaveHeader', notHaveHeader);
+
+    function haveHeaders(haveHeaders) {
+      var headers = this._obj;
+      for(var h in haveHeaders){
+        new chai.Assertion(headers[h]).to.be.eql(haveHeaders[h]);
+      }
+    }
+
+    function notHaveHeader(notHaveHeaders) {
+      var headers = this._obj;
+      notHaveHeaders.forEach(h => {
+        new chai.Assertion(headers[h]).to.be.undefined;
+      });
+    }
+  }
+
 
   function aServer() {
     const server = testkit.server();
     const app = server.getApp();
+    const wixPatchServerResponse = require('wix-patch-server-response');
+    wixPatchServerResponse.patch();
 
-    app.use('/defult', cp.defaultStragegy());
+
+    app.use('/', cp.defaultStrategy());
+    app.use('/default', cp.defaultStrategy());
     app.use('/specific', cp.specific(1000));
-    app.use('/maxAge', cp.maxAge());
-    app.use('/noHeaders', cp.noHeaders());
     app.use('/infinite', cp.infinite());
+    app.use('/noHeaders', cp.noHeaders());
+    app.use('/noCache', cp.noCache());
 
+    app.get('/default', (req, res) => res.send('ok'));
     app.get('/specific', (req, res) => res.send('ok'));
-    app.get('/maxAge', (req, res) => res.send('ok'));
-    app.get('/noHeaders', (req, res) => res.send('ok'));
     app.get('/infinite', (req, res) => res.send('ok'));
+    app.get('/noHeaders', (req, res) => res.send('ok'));
+    app.get('/noCache', (req, res) => res.send('ok'));
 
 
     return server;
