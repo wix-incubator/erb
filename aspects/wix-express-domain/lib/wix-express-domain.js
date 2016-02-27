fc98ac0ec2a62351df5300eb8f9b0c24c138fdd9
Vilius Lukosius <vilius@wix.com>
2016-02-27 16:09:47 +0200

    aspects/wix-express-domain: explicitly binding req/res onto current domain

--- aspects/wix-express-domain/lib/wix-express-domain.js
+++ aspects/wix-express-domain/lib/wix-express-domain.js
@@ -1,4 +1,9 @@
 'use strict';
 const wixDomain = require('wix-domain');
 
-module.exports = (req, res, next) => wixDomain.get().run(next);
+module.exports = (req, res, next) => {
+  const current = wixDomain.get();
+  current.add(req);
+  current.add(res);
+  current.run(next);
+};
