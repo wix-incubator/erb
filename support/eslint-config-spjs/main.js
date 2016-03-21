0df2434fcd07611cd6c104377e7fd895ef871d4a
Vilius Lukosius <vilius@wix.com>
2016-03-21 12:57:15 +0200

    support/eslint-config-spjs: added shared eslint config and initially introduced to /utils/uuid-support

---
+++ support/eslint-config-spjs/main.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true
+  },
+  'globals': {},
+  'rules': {
+    'no-unused-expressions': 2,
+    'no-bitwise': 2,
+    'camelcase': 0,
+    'curly': 2,
+    'eqeqeq': 2,
+    'wrap-iife': [
+      2,
+      'any'
+    ],
+    'indent': [
+      2,
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'new-cap': 2,
+    'no-caller': 2,
+    'quotes': [
+      2,
+      'single'
+    ],
+    'no-undef': 2,
+    'no-unused-vars': 2,
+    'strict': 0,
+    'dot-notation': 0
+  }
+};
---
+++ support/eslint-config-spjs/mocha.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  'env': {
+    'mocha': true
+  },
+  'plugins': [
+    'mocha'
+  ],
+  'rules': {
+    'mocha/no-exclusive-tests': 2
+  }
+};
