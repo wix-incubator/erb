640b8181a78676e3f2f52302cc444c71210a11a0
Aaron Greenwald <aarong@wix.com>
2016-05-04 17:06:28 +0300

    add updateDefaults function to factory

--- bi/wix-bi-logger-client/test/bi-logger-factory.spec.js
+++ bi/wix-bi-logger-client/test/bi-logger-factory.spec.js
@@ -26,6 +26,26 @@ describe('bi-logger-factory', () => {
     });
   });
 
+  describe('updateDefaults', () => {
+    let logger;
+    beforeEach(() => {
+      logger = biLogger.factory().setDefaults({foo: 'bar', baz: 'buzz'});
+    });
+
+    it('should fail when calling with undefined', () => {
+      expect(() => logger.updateDefaults()).to.throw('Defaults must be provided');
+    });
+
+    it('should fail when passing not an object', () => {
+      expect(() => logger.updateDefaults('')).to.throw('Defaults must be an object');
+    });
+
+    it('should allow updating defaults', () => {
+      logger.updateDefaults({baz: 'buzzzzz', newProp: 'value'});
+      expect(logger._defaults).to.deep.equal({foo: 'bar', baz: 'buzzzzz', newProp: 'value'});
+    });
+  });
+
   describe('addEvents', () => {
 
     it('should fail when calling with undefined', () => {
