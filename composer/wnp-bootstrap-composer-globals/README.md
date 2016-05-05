5006aa12ca4fb423231f297f7576b3e371f6d0da
Vilius Lukošius <vilius@wix.com>
2016-05-05 11:31:54 +0300

    composer/wnp-bootstrap-composer-globals: added blank module so ci people could do their thing before weekend

---
+++ composer/wnp-bootstrap-composer-globals/README.md
@@ -0,0 +1,17 @@
+# wnp-bootstrap-composer-globals
+
+TBD
+
+## install
+
+```bash
+npm install --save wnp-bootstrap-composer-globals
+```
+
+## usage
+
+TBD
+
+## api
+
+TBD
---
+++ composer/wnp-bootstrap-composer-globals/test/wnp-bootstrap-composer-globals.it.js
@@ -0,0 +1,9 @@
+'use strict';
+const expect = require('chai').expect;
+
+describe.skip('bootstrap config', () => {
+
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
