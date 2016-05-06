32fbc08c64cfb53c9e35f88bb0f4aad5b194b71e
Vilius Lukosius <vilius@wix.com>
2016-05-06 15:50:30 +0300

    utils/wix-run-mode: fixing test failure in ci #2

--- utils/wix-run-mode/lib/wix-run-mode.js
+++ utils/wix-run-mode/lib/wix-run-mode.js
@@ -2,4 +2,8 @@
 
 module.exports.isDebug = () => process.execArgv.indexOf('--debug') > -1;
 module.exports.isProduction = () => (process.env.NODE_ENV || '').toLowerCase() === 'production';
-module.exports.isCI = () => process.env.IS_BUILD_AGENT ? true : false;
+module.exports.isCI = () => {
+  return !!(process.env.IS_BUILD_AGENT && (process.env.IS_BUILD_AGENT === 'true') || process.env.IS_BUILD_AGENT === true);
+
+
+};
