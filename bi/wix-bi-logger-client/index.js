04a87a1adb9636f263477706c25e31747b49605e
Vilius Lukosius <vilius@wix.com>
2016-06-15 14:50:06 +0300

    bi/wix-bi-logger-client: removed, as migration to bi-logger repo completed

--- bi/wix-bi-logger-client/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const BiLoggerClientFactory = require('./lib/bi-logger-factory'),
-  BiLogger = require('./lib/bi-logger');
-
-module.exports.BiLoggerFactory = BiLoggerClientFactory;
-module.exports.BiLogger = BiLogger;
-
-module.exports.factory = () => new BiLoggerClientFactory();
