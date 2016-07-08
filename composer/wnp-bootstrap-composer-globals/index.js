98a716e2d3b9e03dbb8992d53a4515dcb5e0eca4
Vilius Lukosius <vilius@wix.com>
2016-07-08 08:56:49 +0300

    composer/wnp-bootstrap-composer-globals: removed module as it is no longer needed

--- composer/wnp-bootstrap-composer-globals/index.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-module.exports = require('./lib/wnp-bootstrap-composer-globals');
--- composer/wnp-bootstrap-composer-globals/test/wnp-bootstrap-composer-globals.it.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const expect = require('chai').expect;
-
-describe.skip('bootstrap config', () => {
-
-  it('should pass', () => {
-    expect(true).to.equal(true);
-  });
-});
