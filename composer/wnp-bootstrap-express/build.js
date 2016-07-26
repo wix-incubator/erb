0f2fe818faa89c620c54ae353cb6b87714d9610f
Vilius Lukosius <vilius@wix.com>
2016-07-27 01:33:15 +0300

    removing dual-build from configurable modules

--- composer/wnp-bootstrap-express/build.js
+++
@@ -1,6 +0,0 @@
-const spawn = require('child_process').spawnSync;
-
-const result = spawn('sh', ['-c', 'npm install && node_modules/.bin/spjs-build'], {stdio: 'inherit'});
-if (result.status) {
-  process.exit(result.status);
-}
