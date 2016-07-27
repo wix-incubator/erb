70ca39d537c7fc05f919ab8314c09666dd92cdca
Vilius Lukosius <vilius@wix.com>
2016-07-27 11:07:29 +0300

    support/wnp-configurable: adding option to use function and not script

--- support/wnp-configurable/scripts/wnp-copy-config-templates.js
+++ support/wnp-configurable/scripts/wnp-copy-config-templates.js
@@ -1,28 +1,3 @@
 #!/usr/bin/env node
 'use strict';
-const runMode = require('wix-run-mode'),
-  shelljs = require('shelljs'),
-  join = require('path').join,
-  assert = require('assert');
-
-const logPrefix = `wnp-copy-config-templates(${require(join(process.cwd(), 'package.json')).name}): NODE_ENV='${process.env.NODE_ENV}'`;
-
-if (runMode.isProduction()) {
-  assert(process.env.APP_TEMPL_DIR, 'NODE_ENV=\'production\', but APP_TEMPL_DIR not set, cannot copy config templates.');
-  const targetDir = join(process.env.APP_TEMPL_DIR, '');
-  const srcDir = process.cwd() + '/templates';
-
-  if (!shelljs.test('-d', srcDir)) {
-    console.error(`${logPrefix}, but '${process.cwd()}/templates' does not exist - skipping...`);
-  }
-
-  if (shelljs.ls(srcDir).length === 0) {
-    console.error(`${logPrefix}, but '${process.cwd()}/templates' is empty - skipping...`);
-  }
-
-  console.info(`${logPrefix}, copying configs from '${process.cwd()}/templates' to '${targetDir}'`);
-  shelljs.mkdir('-p', targetDir);
-  shelljs.cp('-R', srcDir + '/', targetDir + '/');
-} else {
-  console.info(`${logPrefix}, skipping...`);
-}
+require('../lib/wnp-copy-config-templates')();
