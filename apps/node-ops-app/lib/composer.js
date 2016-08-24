bbc006777f484412c96a6304940facacd8432041
Vilius Lukosius <vilius@wix.com>
2016-08-24 16:10:54 +0300

    Added statsd port support

--- apps/node-ops-app/lib/composer.js
+++ apps/node-ops-app/lib/composer.js
@@ -1,7 +1,7 @@
 'use strict';
 const host = process.env.NODE_ENV && process.env.NODE_ENV === 'production' ? 'metrics.wixpress.com' : 'metrics.wixpress.com';
 require('appmetrics-statsd').StatsD(
-  '', host, 62003, null, null, null, null, null, ['app_name=node-ops-app', `hostname=${process.env.HOSTNAME || 'localhost'}`]);
+  '', host, 8125, null, null, null, null, null, ['app_name=node-ops-app', `hostname=${process.env.HOSTNAME || 'localhost'}`]);
 
 const Composer = require('wnp-bootstrap-composer').Composer;
 
