7aab719ec2ae8a117bedfeab224d30790a9454bb
Vilius Lukosius <vilius@wix.com>
2016-09-09 11:07:21 +0300

    cluster/wix-new-cluster: lint fix

--- cluster/wix-new-cluster/lib/registry/messages.js
+++ cluster/wix-new-cluster/lib/registry/messages.js
@@ -24,11 +24,3 @@ module.exports.isYouCanDieNow = msg => {
 module.exports.youCanDieNow = workerId => {
   return {channel: 'wix-cluster', id: workerId, type: 'ga-and-die'};
 };
-
-module.exports.isStatsMessage = msg => {
-  return msg && msg.channel && msg.channel === 'wix-cluster' && msg.type && msg.type === 'stats';
-};
-
-module.exports.youCanDieNow = (workerId, stats) => {
-  return {channel: 'wix-cluster', id: workerId, type: 'stats'};
-};
