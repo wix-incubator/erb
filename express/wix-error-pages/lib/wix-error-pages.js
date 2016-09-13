75b90f1716bf7360541924ec32c77c1731ad497d
Amit Shvil <amits@wix.com>
2016-09-13 12:19:23 +0300

    add same changes

--- express/wix-error-pages/lib/wix-error-pages.js
+++ express/wix-error-pages/lib/wix-error-pages.js
@@ -1,10 +1,34 @@
-module.exports = () => {
+const Velocity = require('velocityjs');
+const fs = require('fs');
+
+module.exports = (config) => {
+
+  const templatePath = path.resolve(config.topology.publicStaticBaseUri);
+  let renderModal = {
+    staticsUrl: '//static.parastorage.com/services/'
+  };
   return {
-    render500: () => {
+    render500: (err, req) => {
       return 'Internal Server Error';
     },
-    render504: () => {
+    render504: (err, req) => {
       return 'Gateway Timeout';
     }
   };
-}
+
+  function renderVM(errorModel) {
+    gettingVMFile().then(() => {
+      return Velocity.render(contents, appModel);
+    });
+  }
+
+  function gettingVMFile() {
+    fs.readFile(templatePath, 'utf8', (err, contents) => {
+      if (err) {
+        throw err;
+      }
+      return contents;
+    });
+  }
+};
+
