9166cbaf26e1a6988c7b0fb40fc514a1a8a2814b
Vilius Lukosius <vilius@wix.com>
2016-10-14 10:36:21 +0300

    support/octopus-cli: fixed test

--- support/octopus-cli/test/merge.spec.js
+++ support/octopus-cli/test/merge.spec.js
@@ -5,23 +5,23 @@ const expect = require('chai').expect,
 describe('merge', () => {
 
   it('should update to value from source object', () => {
-    const dest = {foo: "bar"};
-    const source = {foo: "baz"};
+    const dest = {foo: 'bar'};
+    const source = {foo: 'baz'};
 
-    expect(maybeMerge(dest, source)).to.deep.equal({foo: "baz"});
+    expect(maybeMerge(dest, source)).to.deep.equal({foo: 'baz'});
   });
 
   it('should not add elements with keys in source that do not exist in dest', () => {
-    const dest = {foo: "bar"};
-    const source = {fuz: "baz"};
+    const dest = {foo: 'bar'};
+    const source = {fuz: 'baz'};
 
-    expect(maybeMerge(dest, source)).to.deep.equal({foo: "bar"});
+    expect(maybeMerge(dest, source)).to.deep.equal({foo: 'bar'});
   });
 
   it('should deep update to value from source object', () => {
-    const dest = {foo: {foo: "bar"}};
-    const source = {foo: {foo: "baz"}};
+    const dest = {foo: {foo: 'bar'}};
+    const source = {foo: {foo: 'baz'}};
 
-    expect(maybeMerge(dest, source)).to.deep.equal({foo: {foo: "baz"}});
+    expect(maybeMerge(dest, source)).to.deep.equal({foo: {foo: 'baz'}});
   });
 });
