0c4c596aa0b5f0fbf11c49a6f2d3afa9f6351089
Vilius Lukošius <vilius@wix.com>
2016-11-24 09:42:14 +0200

    Revert "bootstrap: migrated to wix-new-cluster" (#388)

--- composer/wnp-bootstrap-runner/test/wnp-bootstrap-runner.spec.js
+++ composer/wnp-bootstrap-runner/test/wnp-bootstrap-runner.spec.js
@@ -1,36 +1,44 @@
 'use strict';
-const expect = require('chai').use(require('sinon-chai')).use(require('chai-as-promised')).expect,
-  sinon = require('sinon'),
-  wixCluster = require('wix-new-cluster'),
+const chai = require('chai'),
+  expect = chai.expect,
+  sinonJs = require('sinon'),
+  wixCluster = require('wix-cluster'),
   runner = require('..'),
   _ = require('lodash');
 
-describe('runner', () => {
-  const match = sinon.match;
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
 
-  it('runs function via wix-cluster and returns resolved promise', sinon.test(function() {
-    this.stub(wixCluster, 'run')
+describe('runner', () => {
+  let sinon;
+  const match = sinonJs.match;
+
+  beforeEach(() => sinon = sinonJs.sandbox.create());
+  afterEach(() => sinon.verifyAndRestore());
+
+  it('runs function via wix-cluster and returns resolved promise', () => {
+    sinon.stub(wixCluster, 'run')
       .withArgs(match.func, undefined)
       .returns(Promise.resolve('done'));
 
     return runner()(() => 'done').then(res => expect(res).to.equal('done'));
-  }));
+  });
 
-  it('should return a failed promise given runnable fails', sinon.test(function() {
-    this.stub(wixCluster, 'run')
+  it('should return a failed promise given runnable fails', () => {
+    sinon.stub(wixCluster, 'run')
       .withArgs(match.func, undefined)
       .returns(Promise.reject(Error('woop')));
 
     return expect(runner()(_.noop)).to.eventually.be.rejectedWith('woop');
-  }));
+  });
 
-  it('should pass-on opts to a wix-cluster run function', sinon.test(function() {
+  it('should pass-on opts to a wix-cluster run function', () => {
     const opts = {forCluster: true};
-    this.mock(wixCluster)
+    sinon.mock(wixCluster)
       .expects('run')
       .withArgs(match.func, opts)
       .once();
 
     return runner(opts)(_.noop);
-  }));
+  });
 });
