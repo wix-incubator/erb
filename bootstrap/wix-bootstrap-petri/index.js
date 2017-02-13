4e1dd3e4fe80470a48bb6a3fb0481b63155f3c56
Vilius Lukosius <vilius@wix.com>
2017-02-13 22:03:44 +0200

    wix-bootstrap-petri, wix-bootstrap-rpc - added ugly deprecation messages

--- bootstrap/wix-bootstrap-petri/index.js
+++ bootstrap/wix-bootstrap-petri/index.js
@@ -1,3 +1,10 @@
+console.log('****************************************************************************');
+console.log('* wix-bootstrap-petri is DEPRECATED                                        *');
+console.log('*                                                                          *');
+console.log('* remove it from your index.js - as .use(require(\'wix-bootstrap-petri\')) *');
+console.log('* and as dependency \'wix-bootstrap-petri\' from package.json              *');
+console.log('*                                                                          *');
+console.log('****************************************************************************');
 module.exports.di = {
   key: '_petri_deprecated',
   value: () => {},
