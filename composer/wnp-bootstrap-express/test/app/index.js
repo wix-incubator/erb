88bbfcfe71d58ba3026e65bfbed67c1fa38386a7
Vilius Lukošius <vilius@wix.com>
2017-03-05 21:45:02 +0200

    *: depcheck and remove unused (#569)

--- composer/wnp-bootstrap-express/test/app/index.js
+++
@@ -1,6 +0,0 @@
-const Composer = require('wnp-bootstrap-composer').Composer;
-
-new Composer({composers: {mainExpress: () => require('../..')({timeout: 1000})}})
-  .express('./test/app/express-app')
-  .express('./test/app/express-app-custom')
-  .start();
