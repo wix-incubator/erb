fd547db028223d19e46a7e12b2bbc392b551ad3a
Vilius Lukošius <vilius@wix.com>
2017-03-06 23:35:40 +0200

    Adding postfix of .gauge to gauges (#577)

--- private/monitoring/wix-measured-statsd-adapter/index.js
+++ private/monitoring/wix-measured-statsd-adapter/index.js
@@ -31,7 +31,7 @@ module.exports = class WixMeasuredStatsdAdapter {
       //TODO: test toFixed
       const res = gauges[key].toJSON();
       if (res) {
-        this._statsdClient.gauge(key, res.toFixed(2));
+        this._statsdClient.gauge(`${key}.gauge`, res.toFixed(2));
       }
     });
   }
@@ -40,9 +40,9 @@ module.exports = class WixMeasuredStatsdAdapter {
     Object.keys(meters).forEach(key => {
       const meter = meters[key].toJSON();
       if (meter.count) {
-        this._statsdClient.gauge(key + '.samples', meter.count);
+        this._statsdClient.gauge(`${key}.samples`, meter.count);
         //TODO: test toFixed
-        this._statsdClient.gauge(key + '.m1_rate', meter['1MinuteRate'].toFixed(2));
+        this._statsdClient.gauge(`${key}.m1_rate`, meter['1MinuteRate'].toFixed(2));
       }
     });
   }
@@ -52,11 +52,11 @@ module.exports = class WixMeasuredStatsdAdapter {
       const hist = hists[key].toJSON();
       if (hist.median) {
         //TODO: test toFixed
-        this._statsdClient.gauge(key + '.max', hist.max.toFixed(2));
-        this._statsdClient.gauge(key + '.p50', hist.median.toFixed(2));
-        this._statsdClient.gauge(key + '.p95', hist.p95.toFixed(2));
-        this._statsdClient.gauge(key + '.p99', hist.p99.toFixed(2));
-        this._statsdClient.gauge(key + '.p999', hist.p999.toFixed(2));
+        this._statsdClient.gauge(`${key}.max`, hist.max.toFixed(2));
+        this._statsdClient.gauge(`${key}.p50`, hist.median.toFixed(2));
+        this._statsdClient.gauge(`${key}.p95`, hist.p95.toFixed(2));
+        this._statsdClient.gauge(`${key}.p99`, hist.p99.toFixed(2));
+        this._statsdClient.gauge(`${key}.p999`, hist.p999.toFixed(2));
       }
     });
   }
