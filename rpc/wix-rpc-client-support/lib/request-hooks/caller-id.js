8deea17925376e92df9aad5de7eb408a37165084
Daniel Shmuglin <daniels@wix.com>
2017-04-13 22:23:45 +0300

    added wixprod.net domain to stripped domains list for caller-id resolution (#670)

--- rpc/wix-rpc-client-support/lib/request-hooks/caller-id.js
+++ rpc/wix-rpc-client-support/lib/request-hooks/caller-id.js
@@ -2,7 +2,8 @@ module.exports.get = callerIdInfo => headers => {
   if (callerIdInfo) {
     const host = callerIdInfo.host
       .replace('.wixpress.com', '')
-      .replace('.wix.com', '');
+      .replace('.wix.com', '')
+      .replace('.wixprod.net', '');
 
     headers['X-Wix-RPC-Caller-ID'] = `${callerIdInfo.artifactId}@${host}`;
   }
--- rpc/wix-rpc-client-support/test/request-hooks/caller-id.spec.js
+++ rpc/wix-rpc-client-support/test/request-hooks/caller-id.spec.js
@@ -2,17 +2,17 @@ const expect = require('chai').expect,
   enricher = require('../../lib/request-hooks/caller-id');
 
 describe('caller-id request-hook', () => {
+  
+  const wixDomains = ['wix.com', 'wixpress.com', 'wixprod.net'];
+  
+  wixDomains.forEach(domain => {
 
-  it('should add header formatted to {artifactId}@{host} and wixpress.com removed', () => {
-    let headers = {};
-    enricher.get({artifactId: 'my-artifact', host: 'my-host.aus.wixpress.com'})(headers);
-    expect(headers['X-Wix-RPC-Caller-ID']).to.equal('my-artifact@my-host.aus');
-  });
-
-  it('should add header formatted to {artifactId}@{host} and wix.com removed', () => {
-    let headers = {};
-    enricher.get({artifactId: 'my-artifact', host: 'my-host.aus.wix.com'})(headers);
-    expect(headers['X-Wix-RPC-Caller-ID']).to.equal('my-artifact@my-host.aus');
+    it(`should add header formatted to {artifactId}@{host} and ${domain} removed`, () => {
+      let headers = {};
+      enricher.get({artifactId: 'my-artifact', host: `my-host.aus.${domain}`})(headers);
+      expect(headers['X-Wix-RPC-Caller-ID']).to.equal('my-artifact@my-host.aus');
+    });
+    
   });
 
   it('should add header formatted to {artifactId}@{host} with original host because is not wix url', () => {
@@ -20,5 +20,4 @@ describe('caller-id request-hook', () => {
     enricher.get({artifactId: 'my-artifact', host: 'my-host.some.com'})(headers);
     expect(headers['X-Wix-RPC-Caller-ID']).to.equal('my-artifact@my-host.some.com');
   });
-
 });
