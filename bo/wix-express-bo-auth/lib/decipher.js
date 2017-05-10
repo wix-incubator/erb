04b5338544888b934912abe6062730df9a98e6c3
Vilius Lukošius <vilius@wix.com>
2017-05-10 10:15:23 +0300

    removed bo as it is moved to a different repo (#719)

--- bo/wix-express-bo-auth/lib/decipher.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('wix-crypto');
-
-module.exports = (token, encryptionKey) => {
-  const decrypted = crypto.decrypt(token, {mainKey: encryptionKey, cipherEncoding: 'base64'});
-  return JSON.parse(decrypted);
-};
-
