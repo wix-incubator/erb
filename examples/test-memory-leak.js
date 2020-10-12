2902431c31086b480207f5f70966b53a3bd9b8f2
Vytautas Jakutis <jakutis@wix.com>
2020-10-12 20:30:56 +0300

    make the code more readable and the memory leak test more visible

--- examples/test-memory-leak.js
+++ examples/test-memory-leak.js
@@ -2,8 +2,14 @@ const assert = require('assert')
 const erb = require('..')
 
 async function main () {
-  for (let i = 0; i < 100000; i++) {
+  let minDuration = Number.POSITIVE_INFINITY
+  let maxMemoryUsed = 0
+  while (true) {
+    const time = Date.now()
     const output = await erb({ template: '<%= TRUE %>' })
+    minDuration = Math.min(minDuration, Date.now() - time)
+    maxMemoryUsed = Math.max(maxMemoryUsed, Math.round(process.memoryUsage().heapUsed / 1024 / 1024))
+    console.log(`maximum ${maxMemoryUsed} megabytes, minimum ${minDuration} milliseconds`)
     assert(output === 'true')
   }
 }
